a2f81c417b973cc00ad3255f7fc102fcf9213e78
Juan Marco <jmmarco@gmail.com>
2019-08-08 17:52:23 -0300

    Fix URI

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,13 +1,13 @@
 export function fetchPopularRepos (language) {
-  const endpoint = window.encodeURI = `https://api.github.com/search/repositories?q=stars>1+language:${language}&sort=stars&order=desc&type=Repositories`
-
+  console.log('Language: ', language)
+  const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+  console.log('fired')
   return fetch(endpoint)
     .then(res => res.json())
     .then(data => {
       if (!data.items) {
         throw new Error(data.message)
       }
-
       return data.items
     })
 }
