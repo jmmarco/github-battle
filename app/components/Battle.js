741d2b4a0282d3b71929a2db4826140679ed456c
Juan Marco <jmmarco@gmail.com>
2019-08-27 19:38:48 -0300

    Add ThemeConsumer and leverage classes to influence styles

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Results from './Results'
+import { ThemeConsumer } from '../contexts/theme'
 
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 
 function Instructions() {
   return (
-    <div className="instuctions-container">
-      <h1 className="center-text heading-lg">Instructions</h1>
-      <ol className="container-sm grid-battle center-text battle-instructions">
-        <li>
-          <h3 className="heading-sm">Enter two Github users</h3>
-          <FaUserFriends className="bg-light" color="rgb(255, 191, 116)" size={240} />
-        </li>
-        <li>
-          <h3 className="heading-sm">Battle</h3>
-          <FaFighterJet className="bg-light" color="#727272" size={240} />
-        </li>
-        <li>
-          <h3 className="heading-sm">See the winners</h3>
-          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={240} />
-        </li>
-      </ol>
-    </div>
+    <ThemeConsumer>
+      {({theme}) => (
+        <div className="instuctions-container">
+          <h1 className="center-text heading-lg">Instructions</h1>
+          <ol className="container-sm grid-battle center-text battle-instructions">
+            <li>
+              <h3 className="heading-sm">Enter two Github users</h3>
+              <FaUserFriends className={`bg-${theme}`} color="rgb(255, 191, 116)" size={240} />
+            </li>
+            <li>
+              <h3 className="heading-sm">Battle</h3>
+              <FaFighterJet className={`bg-${theme}`} color="#727272" size={240} />
+            </li>
+            <li>
+              <h3 className="heading-sm">See the winners</h3>
+              <FaTrophy className={`bg-${theme}`} color="rgb(255, 215, 0)" size={240} />
+            </li>
+          </ol>
+        </div>
+      )}
+    </ThemeConsumer>
   )
 }
 
@@ -52,30 +57,33 @@ class PlayerInput extends React.Component {
 
   render() {
     return (
-
-      <form className="column player" onSubmit={this.handleSubmit}>
-        <label htmlFor='username' className='player-label'>
-          {this.props.label}
-        </label>
-        <div className="row player-inputs">
-          <input
-            type="text"
-            id="username"
-            className="input-light"
-            placeholder="github-username"
-            autoComplete="off"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <button
-            className="btn btn-dark"
-            type="submit"
-            disabled={!this.state.username}
-          >
-          Submit
-          </button>
-        </div>
-      </form>
+      <ThemeConsumer>
+        {({ theme }) => (
+          <form className="column player" onSubmit={this.handleSubmit}>
+            <label htmlFor='username' className='player-label'>
+              {this.props.label}
+            </label>
+            <div className="row player-inputs">
+              <input
+                type="text"
+                id="username"
+                className={`input-${theme}`}
+                placeholder="github-username"
+                autoComplete="off"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <button
+                className={`btn bg-${theme}`}
+                type="submit"
+                disabled={!this.state.username}
+              >
+              Submit
+              </button>
+            </div>
+          </form>
+        )}
+      </ThemeConsumer>
     )
   }
 }
@@ -89,24 +97,28 @@ PlayerInput.propTypes = {
 function PlayerPreview({ username, onReset, label }) {
 
   return (
-    <div className="column player">
-      <h3 className="player-label">{label}</h3>
-      <div className="row bg-light">
-        <div className="player-info ">
-          <img
-            className="avatar-small"
-            src={`https://github.com/${username}.png?size=200`}
-            alt={`Avatar for ${username}`}
-          />
-          <a href={`https://github.com/${username}`}
-            className="link"
-          >{username}</a>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className="column player">
+          <h3 className="player-label">{label}</h3>
+          <div className={`row bg-${theme}`}>
+            <div className="player-info ">
+              <img
+                className="avatar-small"
+                src={`https://github.com/${username}.png?size=200`}
+                alt={`Avatar for ${username}`}
+              />
+              <a href={`https://github.com/${username}`}
+                className="link"
+              >{username}</a>
+            </div>
+            <button className="btn-clear flex-center" onClick={onReset}>
+              <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+            </button>
+          </div>
         </div>
-        <button className="btn-clear flex-center" onClick={onReset}>
-          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
-        </button>
-      </div>
-    </div>
+      )}
+    </ThemeConsumer>
   )
 }
 
--- app/components/Card.js
+++ app/components/Card.js
@@ -1,29 +1,34 @@
 import React from 'react'
+import { ThemeConsumer } from '../contexts/theme'
 import PropTypes from 'prop-types'
 
 export default function Card({ header, subheader, avatar, name, href, children }){
   return (
-    <div className="card bg-light">
-      <h4 className="heading-lg center-text">
-        {header}
-      </h4>
-      <img
-        className="avatar"
-        src={avatar}
-        alt={`Avatar for ${name}`}
-      />
-      {subheader && (
-        <h4 className="center-text">
-          {subheader}
-        </h4>
-        )}
-      <h2 className="center-text">
-        <a className="link" href={href}>
-          {name}
-        </a>
-      </h2>
-      {children}
-    </div>
+    <ThemeConsumer>
+      {({theme}) => (
+        <div className={`card bg-${theme}`}>
+          <h4 className="heading-lg center-text">
+            {header}
+          </h4>
+          <img
+            className="avatar"
+            src={avatar}
+            alt={`Avatar for ${name}`}
+          />
+          {subheader && (
+            <h4 className="center-text">
+              {subheader}
+            </h4>
+            )}
+          <h2 className="center-text">
+            <a className="link" href={href}>
+              {name}
+            </a>
+          </h2>
+          {children}
+        </div>
+      )}
+    </ThemeConsumer>
   )
 }
 
