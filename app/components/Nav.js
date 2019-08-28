1a1d6ee29c38797a6f1034516ab5989a4857650b
Juan Marco <jmmarco@gmail.com>
2019-08-28 11:52:01 -0300

    Use inline styles for simplicity

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { ThemeConsumer } from '../contexts/theme'
+import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
+
   return (
     <ThemeConsumer>
       {({theme, toggleTheme}) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li><NavLink exact activeStyle={{color: 'crimson', fontWeight: 'bold'}} to="/" className="nav-link">Popular</NavLink></li>
+            <li><NavLink activeStyle={{color: 'crimson', fontWeight: 'bold'}} to="/battle" className="nav-link">Battle</NavLink></li>
+          </ul>
           <button
             style={{fontSize: 30}}
             className="btn-clear"
