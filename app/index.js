a6d42ea4b2184caf8890a41e8f31438d74ed1b13
Juan Marco <jmmarco@gmail.com>
2019-08-28 11:53:22 -0300

    Apply Router component and attach class to main app container

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // Component
 // State
@@ -26,14 +27,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme === 'light' ? 'light' : 'dark'}>
-          <React.Fragment>
-            <Nav />
-            <Popular />
-          </React.Fragment>
-        </div>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={`${this.state.theme === 'light' ? 'light' : 'dark'} container`}>
+            <React.Fragment>
+              <Nav />
+              <Route exact path="/" component={Popular} />
+              <Route path="/battle" component={Battle} />
+            </React.Fragment>
+          </div>
+        </ThemeProvider>
+      </Router>
     )
   }
   // This will get converted to:
