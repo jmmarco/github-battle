54b310dcca5b16b772f497e9f47c8e6009d4d90f
Juan Marco <jmmarco@gmail.com>
2019-08-29 19:54:14 -0300

    Add Switch for 404 handling

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Results from './components/Results'
 
 // Component
@@ -28,18 +28,27 @@ class App extends React.Component {
 
   render() {
     return (
+
       <Router>
         <ThemeProvider value={this.state}>
           <div className={`${this.state.theme === 'light' ? 'light' : 'dark'} container`}>
             <React.Fragment>
               <Nav />
-              <Route exact path="/" component={Popular} />
-              <Route exact path='/battle' component={Battle} />
-              <Route path='/battle/results' component={Results} />
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path='/battle' component={Battle} />
+                <Route path='/battle/results' component={Results} />
+                <Route render={() => {
+                  return (
+                      <h1 style={{ textAlign: 'center' }}>Sorry ❗⛔ ❗That page does not exist!</h1>
+                  )
+                }} />
+              </Switch>
             </React.Fragment>
           </div>
         </ThemeProvider>
       </Router>
+
     )
   }
   // This will get converted to:
