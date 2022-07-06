265afa7514727c6f6c4843bae4803e12bda04949
Juan Marco <jmmarco@gmail.com>
2022-07-06 18:02:31 +0200

    Refactor Tooltip for useHover

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Hover from './Hover'
+import React from 'react';
+import PropTypes from 'prop-types';
+import useHover from '../hooks/useHover';
 
 const styles = {
   container: {
     position: 'relative',
-    display: 'flex'
+    display: 'flex',
   },
   tooltip: {
     boxSizing: 'border-box',
@@ -21,22 +21,21 @@ const styles = {
     color: '#fff',
     textAlign: 'center',
     fontSize: '14px',
-  }
-}
+  },
+};
 
-export default function Tooltip ({ text, children }) {
+export default function Tooltip({ text, children }) {
+  const [hovering, attrs] = useHover();
+
   return (
-    <Hover>
-      {(hovering) => (
-        <div style={styles.container}>
-          {hovering === true && <div style={styles.tooltip}>{text}</div>}
-          {children}
-        </div>
-      )}
-    </Hover>
-  )
+    <div style={styles.container} {...attrs}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  );
 }
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
-}
+  children: PropTypes.array.isRequired,
+};
