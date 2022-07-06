70406734975cd43a4e79b0b18bbabb8913e174fa
Juan Marco <jmmarco@gmail.com>
2022-07-06 11:56:42 +0200

    Refactor Loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const styles = {
   content: {
@@ -9,38 +9,33 @@ const styles = {
     right: '0',
     marginTop: '20px',
     textAlign: 'center',
-  }
-}
+  },
+};
 
-export default class Loading extends React.Component {
-  state = { content: this.props.text }
-  componentDidMount () {
-    const { speed, text } = this.props
+export default function Loading({ speed, text }) {
+  const [content, setContent] = useState(() => text);
 
-    this.interval = window.setInterval(() => {
-      this.state.content === text + '...'
-        ? this.setState({ content: text })
-        : this.setState(({ content }) => ({ content: content + '.' }))
-    }, speed)
-  }
-  componentWillUnmount () {
-    window.clearInterval(this.interval)
-  }
-  render() {
-    return (
-      <p style={styles.content}>
-        {this.state.content}
-      </p>
-    )
-  }
+  const intervalId = useRef(null);
+
+  useEffect(() => {
+    intervalId.current = window.setInterval(() => {
+      content === text + '...'
+        ? setContent(text)
+        : setContent((content) => content + '.');
+    }, speed);
+
+    return () => window.clearInterval(intervalId.current);
+  }, [speed, text]);
+
+  return <p style={styles.content}>{content}</p>;
 }
 
 Loading.propTypes = {
   text: PropTypes.string.isRequired,
   speed: PropTypes.number.isRequired,
-}
+};
 
 Loading.defaultProps = {
   text: 'Loading',
-  speed: 300
-}
+  speed: 300,
+};
